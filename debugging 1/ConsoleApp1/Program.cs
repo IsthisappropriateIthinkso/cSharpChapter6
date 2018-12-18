9de7ffccd6654eecccb9a5b8b53de4fb039824d9
IsthisappropriateIthinkso <ag137220@students.davenport.k12.ia.us>
2018-12-17 21:50:18 -0600

    Add files via upload

---
+++ debugging 1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+class DebugSix01
+{
+   static void Main()
+   {
+      string[] items = {"Belt", "Tie", "Scarf", "Cufflinks"};
+      double[] prices = {29.00, 35.95, 18.50, 112.99};
+        WriteLine("Items for sale:");
+        Console.WriteLine("");
+        for (int x = 0; x < 4; x++)
+        {
+  
+            WriteLine("{0} for {1}",
+          items[x], prices[x].ToString("C"));
+        }
+       
+   }
+}
---
+++ debugging 4/debugging 4/Program.cs
@@ -0,0 +1,39 @@
+﻿
+// Program prompts user for any number of values 
+// (up to 20)
+// and averages them
+using System;
+using static System.Console;
+class DebugSix04
+{
+    public static void Main()
+    {
+        const int QUIT = 999;
+        int[] numbers = new int[20];
+        int x;
+        int num;
+        double average;
+        double total = 0;
+        string inString;
+        Console.Write("Please enter a number or " +QUIT + " to quit...");
+        inString = ReadLine();
+        num = Convert.ToInt32(inString);
+        while ((num < numbers.Length) && num != QUIT)
+
+        {
+            numbers[num] = num;
+            total += numbers[num];
+            ++num;
+            Write("Please enter a number or " +
+               QUIT + " to quit...");
+            inString = ReadLine();
+            num = Convert.ToInt32(inString);
+        }
+        WriteLine("The numbers are:");
+        for (int y = 0; y < num; ++num)
+            Console.Write("{0,6}", numbers[y]);
+        average = total / num;
+        WriteLine("");
+        WriteLine("The average is {0}", average);
+    }
+}
